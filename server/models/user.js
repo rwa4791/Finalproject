0e625efef855267f64903e302c9f44ef145899bb
hcandelaria <hcandelaria66.7@gmail.com>
2018-02-05 21:13:39 -0500

    deploy to heroku

--- server/models/user.js
+++ server/models/user.js
@@ -21,20 +21,20 @@ UserSchema.methods.comparePassword = function comparePassword(password, callback
  * The pre-save hook method.
  */
 UserSchema.pre('save', function saveHook(next) {
-  const user = this;
+  const User = this;
 
-  // proceed further only if the password is modified or the user is new
-  if (!user.isModified('password')) return next();
+  // proceed further only if the password is modified or the User is new
+  if (!User.isModified('password')) return next();
 
 
   return bcrypt.genSalt((saltError, salt) => {
     if (saltError) { return next(saltError); }
 
-    return bcrypt.hash(user.password, salt, (hashError, hash) => {
+    return bcrypt.hash(User.password, salt, (hashError, hash) => {
       if (hashError) { return next(hashError); }
 
       // replace a password string with hash value
-      user.password = hash;
+      User.password = hash;
 
       return next();
     });
